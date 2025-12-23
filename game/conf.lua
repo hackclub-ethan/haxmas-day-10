2b807461cbe4d19308ff49891631f7268f1223f0
hackclub-ethan <195668068+hackclub-ethan@users.noreply.github.com>
2025-12-22 23:17:24 -0500

    All the stuff

---
+++ game/conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = "Haxmas game :3" -- change this to your game name
+    t.version = "11.4"
+    t.window.width = 800
+    t.window.height = 400
+    t.window.resizable = false
+end
